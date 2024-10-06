7b65de579568b925bfe0265653ad878e8700162f
Daniel Tom <d.e.tom89@gmail.com>
2024-10-06 22:17:15 +0200

    Add output type json and ascii

--- src/binary_cookies_parser/__main__.py
+++ src/binary_cookies_parser/__main__.py
@@ -1,12 +1,40 @@
+import json
+from datetime import datetime
+from enum import Enum
+from typing import Type
+
 import typer
 from rich import print
 
 from binary_cookies_parser.parser import read_binary_cookies_file
 
 
-def cli(file_path: str):
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, obj: Type) -> str:
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        return super().default(obj)
+
+
+class OutputType(str, Enum):
+    json = "json"
+    ascii = "ascii"
+
+
+def cli(file_path: str, output: OutputType = "json"):
     cookies = read_binary_cookies_file(file_path)
-    [print(cookie.json()) for cookie in cookies]
+    if output == OutputType.json:
+        print(json.dumps([cookie.model_dump() for cookie in cookies], indent=2, cls=DateTimeEncoder))
+    elif output == OutputType.ascii:
+        for cookie in cookies:
+            print(f"Name: {cookie.name}")
+            print(f"Value: {cookie.value}")
+            print(f"URL: {cookie.url}")
+            print(f"Path: {cookie.path}")
+            print(f"Created: {cookie.create_datetime.isoformat()}")
+            print(f"Expires: {cookie.expiry_datetime.isoformat()}")
+            print(f"Flag: {cookie.flag}")
+            print("-" * 40)
 
 
 def main():
