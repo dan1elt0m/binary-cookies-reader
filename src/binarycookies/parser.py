329a084d90f01ff9a979710c7e1959beea4399ab
Daniel Tom <d.e.tom89@gmail.com>
2025-04-08 22:08:27 +0200

    Update deprecation warning message

--- src/binarycookies/parser.py
+++ src/binarycookies/parser.py
@@ -6,7 +6,7 @@ from binarycookies._deserialize import load
 from binarycookies.models import Cookie
 
 
-@deprecated("read_binary_cookies_file is deprecated, use binary_cookies_parser.load instead.")
+@deprecated("read_binary_cookies_file is deprecated, use binarycookies.load instead.")
 def read_binary_cookies_file(file_path: str) -> List[Cookie]:
     with open(file_path, "rb") as f:
         return load(f)
